c3d46e29dea53746245bd7214d4d9af5f42ce2e9
nsobo <n.sobo@vegaitsourcing.rs>
2016-12-14 22:36:10 +0100

    Uradjena forma za prvi korak kupovine osiguranja.

---
+++ SEP/SEP.WEBApi/App/Components/RestServices/merchant-sport.service.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    angular.module('sport-module').factory('sportService', sportService);
+
+    sportService.$inject = ['$resource'];
+    function sportService($resource) {
+        var url = "http://travelinsurace.local/insurance/sport";
+        return $resource(url, {});
+    }
+})
